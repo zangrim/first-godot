0cc3c9b061cec79ae21b83abe2805cf60ee7c99b
zangrim <grimzan9@gmail.com>
2024-05-13 13:11:20 +0800

    Adding Heads up display

--- main.gd
+++ main.gd
@@ -17,12 +17,16 @@ func _process(delta):
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 
 func _on_mob_timer_timeout():
@@ -53,6 +57,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
